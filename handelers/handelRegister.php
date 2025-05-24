2f458790891981836a308e698f8866f4cb6220de
Abdelrahman Altayeb <38187446+Maaaann@users.noreply.github.com>
2025-05-24 23:31:39 +0300

    Filter the input by the "FOREACH"

--- handelers/handelRegister.php
+++ handelers/handelRegister.php
@@ -3,10 +3,14 @@
 include "../core/functions.php";
 
 if(checkRequestMethod("POST") && checkMethodInput("name")){
-    $name = filterInput($_POST['name']);
-    $email = filterInput($_POST['email']);
-    $password = filterInput($_POST['password']);
-
+    foreach($_POST as $key => $value){
+        $$key = filterInput($value);
+    }
+    
+    // $name = filterInput($_POST['name']);
+    // $email = filterInput($_POST['email']);
+    // $password = filterInput($_POST['password']);
+   
 
 }else{
     echo "Server error!";
