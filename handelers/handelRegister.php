85fc8a9ed737bdf0236e83a3d60d8afb4e565a56
Abdelrahman Altayeb <38187446+Maaaann@users.noreply.github.com>
2025-05-26 17:32:23 +0300

    The error message for wrong input
    Finally oash

--- handelers/handelRegister.php
+++ handelers/handelRegister.php
@@ -1,9 +1,11 @@
 <?php
-
+session_start();
 include "../core/functions.php";
 include "../core/validation.php";
+$errors=[];
 
-if(checkRequestMethod("POST") && checkMethodInput("name")){
+if(checkRequestMethod("POST") && checkMethodInput("name"))
+{
     foreach($_POST as $key => $value){
         $$key = filterInput($value);
     }
@@ -11,14 +13,20 @@ if(checkRequestMethod("POST") && checkMethodInput("name")){
 
     // Name Validation
     if(!required($name)){
-        echo"Name is required";
+        $errors[]= "Name is required";
     }elseif(!minimumVal($name,3)){
-        echo "Minimum Value for the name input is 3";
+        $errors[]= "Minimum Value for the name input is 3";
     }elseif(!maximumVal($name,20)){
-        echo "Sorry Maximum Value mus be smaller than 20 chars";
+        $errors[]= "Sorry Maximum Value mus be smaller than 20 chars";
     }
-        
-
+    
+    if(empty($errors)){
+        echo "DONE!";
+    }else{
+        $_SESSION['errors'] = $errors;
+        header("location:../register.php");
+        die;
+    }
 
 
 }else{
--- register.php
+++ register.php
@@ -5,7 +5,17 @@
     <div class="row">
         <div class="col-8 mx-auto my-5">
         <h2 class="border p-2 my-2 text-center">Register</h2>
-
+            <?php
+                if(isset($_SESSION['errors'])):
+                    foreach($_SESSION['errors'] as $error): ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $error ; ?>
+                        </div>
+            <?php 
+                    endforeach;
+                    unset($_SESSION['errors']);
+                endif; 
+            ?> 
             <form action="handelers/handelRegister.php" method="POST" class="border p-3">
                 <div class="form-group p-2 my-1">
                     <label for="name">Name</label>
