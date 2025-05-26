dce754d7c5079ab1a9a5bcc108cac11f68bce320
Abdelrahman Altayeb <38187446+Maaaann@users.noreply.github.com>
2025-05-26 19:41:46 +0300

    the profile show data

--- profile.php
+++ profile.php
@@ -1,7 +1,14 @@
 <?php include __DIR__."/inc/header.php";?>
 <?php include __DIR__."/inc/nav.php";?>
 
-
+<div class="container">
+    <div class="row">
+        <div class="col-8 mx-auto my-5 border p-2">
+            <h2 class="border my-2 ">name: <?php echo $_SESSION['auth'][0]?></h2>
+            <h2 class="border my-2 ">email:<?php echo $_SESSION['auth'][1]?></h2>
+        </div>
+    </div>
+</div>
 
 
 
